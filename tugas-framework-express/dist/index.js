9f226053aa0251d8d6966ccea27e0267ee65711e
Albar Oktaha <albaroktahaa@gmail.com>
2024-11-05 20:12:10 +0700

    Tugas Framework Express

---
+++ tugas-framework-express/dist/index.js
@@ -0,0 +1,47 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const path = require('path');
+const PORT = 3000;
+function init() {
+    const app = (0, express_1.default)();
+    // Middleware untuk log setiap permintaan
+    app.use((req, res, next) => {
+        console.log(`${req.method} request for '${req.url}'`);
+        next();
+    });
+    app.get("/", (req, res) => {
+        res.status(200).json({
+            message: "OK",
+            data: null,
+        });
+    });
+    // Route untuk /hello
+    app.get('/hello', (req, res) => {
+        res.json({
+            message: 'Success fetch message',
+            data: 'Hello World!'
+        });
+    });
+    // Route untuk /user
+    app.get('/user', (req, res) => {
+        res.json({
+            message: 'Success fetch user',
+            data: {
+                id: 1,
+                name: 'Budi',
+                username: 'budidu',
+                email: 'budidu@mail.com'
+            }
+        });
+    });
+    // Melayani file statis dari direktori "public"
+    app.use(express_1.default.static('public'));
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
+}
+init();
