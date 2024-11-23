c425d2cb25f647d6fca8babc0b6e66185285839b
Albar Oktaha <albaroktahaa@gmail.com>
2024-11-24 00:38:55 +0700

    Tugas Validation, Mongoose Middleware dan Pagination Query

--- tugas-mongoose-odm/src/controllers/products.controller.ts
+++ tugas-mongoose-odm/src/controllers/products.controller.ts
@@ -1,15 +1,46 @@
+/**
+ * src/controllers/product.controller.ts
+ */
 import { Request, Response } from "express";
-import ProductsModel from "@/models/products.model";
+import * as Yup from "yup";
+import ProductsModel from "../models/products.model";
+
+import {
+  create,
+  findAll,
+  findOne,
+  update,
+  remove,
+} from "../services/product.service";
+
+const createValidationSchema = Yup.object().shape({
+  name: Yup.string().required(),
+  price: Yup.number().required(),
+  category: Yup.string().required(),
+  description: Yup.string().required(),
+  images: Yup.array().of(Yup.string()).required().min(1),
+  qty: Yup.number().required().min(1),
+});
+
+import {IPaginationQuery} from '../utils/interfaces';
 
 export default {
   async create(req: Request, res: Response) {
     try {
-      const result = await ProductsModel.create(req.body);
+      await createValidationSchema.validate(req.body);
+      const result = await create(req.body);
       res.status(201).json({
         data: result,
         message: "Success create product",
       });
     } catch (error) {
+      if (error instanceof Yup.ValidationError) {
+        res.status(400).json({
+          data: error.errors,
+          message: "Failed create product",
+        });
+        return;
+      }
       const err = error as Error;
       res.status(500).json({
         data: err.message,
@@ -19,10 +50,30 @@ export default {
   },
   async findAll(req: Request, res: Response) {
     try {
-      const result = await ProductsModel.find();
+      const {
+        limit = 10,
+        page = 1,
+        search = "",
+      } = req.query as unknown as IPaginationQuery;
+      const query = {};
+      if (search) {
+        Object.assign(query, {
+          name: { $regex: search, $options: "i" },
+        });
+      }
+      const result = await ProductsModel.find(query)
+        .limit(limit)
+        .skip((page - 1) * limit)
+        .sort({ createdAt: -1 })
+        .populate("category");
+      const total = await ProductsModel.countDocuments(query);
       res.status(200).json({
         data: result,
         message: "Success get all products",
+        page: +page,
+        limit: +limit,
+        total,
+        totalPages: Math.ceil(total / limit),
       });
     } catch (error) {
       const err = error as Error;
@@ -34,9 +85,8 @@ export default {
   },
   async findOne(req: Request, res: Response) {
     try {
-      const result = await ProductsModel.findOne({
-        _id: req.params.id,
-      });
+      const result = await findOne(req.params?.id);
+
       res.status(200).json({
         data: result,
         message: "Success get one product",
@@ -51,13 +101,7 @@ export default {
   },
   async update(req: Request, res: Response) {
     try {
-      const result = await ProductsModel.findOneAndUpdate(
-        { _id: req.params.id },
-        req.body,
-        {
-          new: true,
-        }
-      );
+      const result = await update(req.params?.id, req.body);
 
       res.status(200).json({
         data: result,
@@ -73,9 +117,7 @@ export default {
   },
   async delete(req: Request, res: Response) {
     try {
-      const result = await ProductsModel.findOneAndDelete({
-        _id: req.params.id,
-      });
+      const result = await remove(req.params?.id);
 
       res.status(200).json({
         data: result,
--- tugas-mongoose-odm/src/models/products.model.ts
+++ tugas-mongoose-odm/src/models/products.model.ts
@@ -1,31 +1,44 @@
-import mongoose from "mongoose";
+import mongoose, { Types } from "mongoose";
+
+export interface Product {
+  name: string;
+  description: string;
+  price: number;
+  qty: number;
+  slug: string;
+  categoryId: Types.ObjectId;
+  createdAt: string;
+  updatedAt: string;
+  _id?: Types.ObjectId;
+}
 
 const Schema = mongoose.Schema;
 
-const ProductsSchema = new Schema(
+const ProductsSchema = new Schema<Product>(
   {
     name: {
-      type: String,
+      type: Schema.Types.String,
       required: true,
     },
     description: {
-      type: String,
-      required: true,
-    },
-    images: {
-      type: [String],
+      type: Schema.Types.String,
       required: true,
     },
     price: {
-      type: Number,
+      type: Schema.Types.Number,
       required: true,
     },
     qty: {
-      type: Number,
+      type: Schema.Types.Number,
       required: true,
+      min: [1, "Minimal qty adalah 1"],
+    },
+    slug: {
+      type: String,
+      unique: true,
     },
     categoryId: {
-      type: mongoose.Schema.Types.ObjectId,
+      type: Schema.Types.ObjectId,
       ref: "Category",
     },
   },
@@ -33,6 +46,14 @@ const ProductsSchema = new Schema(
     timestamps: true,
   }
 );
+
+ProductsSchema.pre("save", function (next) {
+  const product = this;
+  if (!product.slug) {
+    product.slug = product.name.toLowerCase().split(" ").join("-");
+  }
+  next();
+});
 
 const ProductsModel = mongoose.model("Products", ProductsSchema);
 
