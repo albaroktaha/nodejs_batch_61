99b8e6a0364c82d568ff141cbcd79d77f9511109
Albar Oktaha <albaroktahaa@gmail.com>
2024-11-24 00:53:19 +0700

    API Authorization

--- tugas-service-pattern/src/routes/api.ts
+++ tugas-service-pattern/src/routes/api.ts
@@ -8,13 +8,15 @@ import productsController from "../controllers/products.controller";
 import categoriesController from "../controllers/categories.controller";
 import authController from "../controllers/auth.controller";
 import authMiddleware from "../middlewares/auth.middlewre";
+import rbacMiddleware from "../middlewares/rbac.middleware";
 
 const router = express.Router();
 
 // Auth
 router.post("/auth/login", authController.login);
 router.post("/auth/register", authController.register);
-router.post("/auth/me", authMiddleware, authController.me);
+// router.post("/auth/me", authMiddleware, authController.me);
+router.post("/auth/me", [authMiddleware, rbacMiddleware(["admin"])], authController.me);
 router.put("/auth/update-profile", authMiddleware, authController.profile);
 
 // CRUD Categories
